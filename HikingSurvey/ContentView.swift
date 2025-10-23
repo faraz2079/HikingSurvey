e09c7b772644985f309dce1f06f7bc3d1be601e8
faraz2079 <farazchahi2079@gmail.com>
2025-10-23 12:03:59 +0200

    Initial Commit

---
+++ HikingSurvey/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HikingSurvey
+//
+//  Created by Faraz on 10/23/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Opinions on Hiking")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HikingSurvey/HikingSurveyApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikingSurveyApp.swift
+//  HikingSurvey
+//
+//  Created by Faraz on 10/23/25.
+//
+
+import SwiftUI
+
+@main
+struct HikingSurveyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
