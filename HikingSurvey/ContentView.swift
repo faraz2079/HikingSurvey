01268cab2ba261a60171196bdb4d592cbbc6b63b
faraz2079 <farazchahi2079@gmail.com>
2025-10-23 12:18:08 +0200

    Response class added + scrollview and data being shown on the screen

--- HikingSurvey/ContentView.swift
+++ HikingSurvey/ContentView.swift
@@ -8,9 +8,25 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var responses: [Response] = []
+    
     var body: some View {
         VStack {
             Text("Opinions on Hiking")
+                .frame(maxWidth: .infinity)
+                .font(.title)
+                .padding(.top, 24)
+            ScrollView {
+                ForEach(responses) { response in
+                    Text(response.text)
+                }
+            }
+        }
+        .onAppear {
+            for response in Response.sampleResponses {
+                responses.insert(Response(text: response), at: 0)
+            }
         }
         .padding()
     }
