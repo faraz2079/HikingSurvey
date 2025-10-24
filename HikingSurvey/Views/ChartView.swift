ce9702bdb63b24ba606ac3683c8d54da9874d988
faraz2079 <farazchahi2079@gmail.com>
2025-10-24 15:44:34 +0200

    ChartView changes to donutChart + to the hiking image

--- HikingSurvey/Views/ChartView.swift
+++ HikingSurvey/Views/ChartView.swift
@@ -18,7 +18,7 @@ struct ChartView: View {
     
     var body: some View {
         Chart(responses) { response in
-            SectorMark(angle: .value("Type", 1)) //, angularInset: 1
+            SectorMark(angle: .value("Type", 1), innerRadius: .ratio(0.75)) //, angularInset: 1
                 .foregroundStyle(by: .value("sentiment", response.sentiment))
         }
         .chartForegroundStyleScale([
@@ -26,6 +26,19 @@ struct ChartView: View {
             Sentiment.negative : Sentiment.negative.sentimentColor,
             Sentiment.moderate : Sentiment.moderate.sentimentColor
         ])
+        .chartBackground(content: { chartProxy in
+            GeometryReader { geometry in
+                if let anchor = chartProxy.plotFrame {
+                    let frame = geometry[anchor]
+                    Image(systemName: "figure.hiking")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(height: frame.height * 0.4)
+                        .foregroundStyle(Color(white: 0.59))
+                        .position(x: frame.midX, y: frame.midY)
+                }
+            }
+        })
         .chartLegend(position: .trailing, alignment: .center)
         .frame(height: 200)
         .padding()
