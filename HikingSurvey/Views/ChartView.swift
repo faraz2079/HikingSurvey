daa1ba374fd7a3cc6f4190cd035f52c50c9b3887
faraz2079 <farazchahi2079@gmail.com>
2025-10-24 14:45:24 +0200

    ChartView class added

---
+++ HikingSurvey/Views/ChartView.swift
@@ -0,0 +1,22 @@
+//
+//  ChartView.swift
+//  HikingSurvey
+//
+//  Created by Faraz on 10/24/25.
+//
+
+import SwiftUI
+import Charts
+
+struct ChartView: View {
+    
+    var responses: [Response]
+    
+    var body: some View {
+        
+    }
+}
+
+#Preview {
+    ContentView()
+}
